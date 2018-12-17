c1239291cfabfe63e6fd7ca109a135753f9fa249
Sang Au <auvansang@outlook.com>
2018-12-17 18:18:20 +0700

    Add bootstrap to use new theme api

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
+import './bootstrap';
+
 import React from 'react';
 import { render } from 'react-dom';
 
 import { CssBaseline } from '@material-ui/core';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import { StylesProvider, ThemeProvider } from '@material-ui/styles';
-import { theme, generateClassName } from './theme';
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './theme';
 
 import AppProvider from 'utils/store/AppProvider';
 import { store } from './store';
@@ -13,21 +14,17 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 function renderApp() {
-	const rootElement = document.getElementById('root')!;
+  const rootElement = document.getElementById('root')!;
 
-	render(
-		<AppProvider store={store}>
-			<MuiThemeProvider theme={theme}>
-				<StylesProvider generateClassName={generateClassName}>
-					<ThemeProvider theme={theme}>
-						<CssBaseline />
-						<App />
-					</ThemeProvider>
-				</StylesProvider>
-			</MuiThemeProvider>
-		</AppProvider>,
-		rootElement
-	);
+  render(
+    <AppProvider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </AppProvider>,
+    rootElement
+  );
 }
 
 renderApp();
@@ -35,5 +32,5 @@ renderApp();
 serviceWorker.unregister();
 
 if (module.hot) {
-	module.hot.accept('./App', renderApp);
+  module.hot.accept('./App', renderApp);
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,71 +1,66 @@
-import { createMuiTheme, createGenerateClassName } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
 
 // create a theme instance.
 const theme = createMuiTheme({
-	props: {
-		MuiButtonBase: {
-			disableRipple: true
-		},
-		MuiListItem: {
-			disableRipple: true
-		},
-		MuiListItemIcon: {
-			style: {
-				marginRight: 0
-			}
-		}
-	},
-	palette: {
-		primary: {
-			light: '#26c281',
-			main: '#3fc380',
-			dark: '#4daf7c',
-			contrastText: '#fff'
-		},
-		secondary: {
-			light: '#29f1c3',
-			main: '#36d7b7',
-			dark: '#4ecdc4',
-			contrastText: '#fff'
-		},
-		error: {
-			light: '#f89406',
-			main: '#f9690e',
-			dark: '#d35400',
-			contrastText: '#fff'
-		},
-		text: {
-			primary: '#24252a',
-			secondary: '#2e3131',
-			disabled: '#dadfe1',
-			hint: '#abb7b7'
-		}
-	},
-	typography: {
-		fontFamily: '"Quicksand", sans-serif;',
-		button: {
-			textTransform: 'capitalize'
-		},
-		useNextVariants: true
-	},
-	shape: {
-		borderRadius: 0
-	},
-	overrides: {
-		MuiSvgIcon: {
-			root: {
-				fontSize: 20
-			}
-		}
-	},
-	transitions: {
-		create: () => 'none'
-	}
+  props: {
+    MuiButtonBase: {
+      disableRipple: true
+    },
+    MuiListItem: {
+      disableRipple: true
+    },
+    MuiListItemIcon: {
+      style: {
+        marginRight: 0
+      }
+    }
+  },
+  palette: {
+    primary: {
+      light: '#26c281',
+      main: '#3fc380',
+      dark: '#4daf7c',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#29f1c3',
+      main: '#36d7b7',
+      dark: '#4ecdc4',
+      contrastText: '#fff'
+    },
+    error: {
+      light: '#f89406',
+      main: '#f9690e',
+      dark: '#d35400',
+      contrastText: '#fff'
+    },
+    text: {
+      primary: '#24252a',
+      secondary: '#2e3131',
+      disabled: '#dadfe1',
+      hint: '#abb7b7'
+    }
+  },
+  typography: {
+    fontFamily: '"Quicksand", sans-serif;',
+    button: {
+      textTransform: 'capitalize'
+    },
+    useNextVariants: true
+  },
+  shape: {
+    borderRadius: 0
+  },
+  overrides: {
+    MuiSvgIcon: {
+      root: {
+        fontSize: 20
+      }
+    }
+  },
+  transitions: {
+    create: () => 'none'
+  }
 });
 
-const generateClassName = createGenerateClassName({
-	dangerouslyUseGlobalCSS: true,
-	productionPrefix: 'mt'
-});
-
-export { theme, generateClassName };
+export default theme;
