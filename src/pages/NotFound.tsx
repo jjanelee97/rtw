0ecfefeb633d430de3b200da6fcd4ade2dfc3fe4
Sang Au <auvansang@outlook.com>
2018-12-18 23:19:10 +0700

    Update packages

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,27 +2,27 @@ import React from 'react';
 import Helmet from 'react-helmet';
 
 type Props = {
-	location: Location;
+  location: Location;
 };
 
 const NotFound = (props: Props) => {
-	return (
-		<>
-			<Helmet
-				title={'404 Not found - React with Typescript and Webpack'}
-				meta={[
-					{
-						name: 'description',
-						content: '404 Not found - React with Typescript and Webpack'
-					}
-				]}
-			/>
-			<h1>404 Not found</h1>
-			<h2>
-				No match found for <code>{props.location.pathname}</code>
-			</h2>
-		</>
-	);
+  return (
+    <>
+      <Helmet
+        title={'404 Page Not found - React with Typescript and Webpack'}
+        meta={[
+          {
+            name: 'description',
+            content: '404 Page Not found - React with Typescript and Webpack'
+          }
+        ]}
+      />
+      <h1>404 Page Not found</h1>
+      <h2>
+        No match found for <code>{props.location.pathname}</code>
+      </h2>
+    </>
+  );
 };
 
 export default NotFound;
